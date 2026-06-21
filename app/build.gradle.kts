73e89ef046ccc1d05cceb3849b30542abf22c48c
dchlfin <167969296+dchlfin@users.noreply.github.com>
2026-06-21 22:10:08 +0400

    fix: remove redundant launcher.xml file to fix gradle bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,6 +31,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    packaging {
+        jniLibs {
+            pickFirsts += "**/libc++_shared.so"
+        }
+    }
 }
 
 dependencies {
@@ -40,6 +45,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.cardview)
+    implementation("androidx.core:core-splashscreen:1.2.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
